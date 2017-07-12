acede2044f6ca8cda232a059ad05b35ccc804110
Alex Wang <aw1621107@gmail.com>
2017-07-12 00:36:36 -0400

    Move stage 1 to lower memory before loading starts
    Moves the yet-to-be-executed stage 1 code to a lower place in memory.
    This is normally used for chainloading, which likely isn't going to be
    implemented any time soon, but has the potentially convenient side
    effect of making the usable memory contiguous, instead of having some
    space, then the bootloader, then more space. Doubt I'll be using all
    that space any time soon, but it might be possible to generalize this
    code to be able to copy a more complex/memory-hungry bootloader to lower
    memory, where not having to deal with a "hole" in available memory would
    be nice.
    
    The target address of 0x0060:0x000 was chosen because the OSDev wiki
    claimed that that was the "traditional" address the MBR is usually
    relocated to when chainloading.
    
    Not copying all 512 bytes of the bootloader code isn't strictly
    necessary, but I just felt like it, so I did. The optimization doesn't
    consume a significantly greater amount of space (I think), and is still
    relatively easy to understand (I hope). Saving a few cycles may not
    really matter with hardware these days, but this is a kernel, so might
    as well eke out performance where I can.
    
    In any case, I think some math would be necessary to ensure that the CPU
    jumps to the actual stage 1 code instead of the relocation code again,
    so it can be argued that this is simpler.
    
    Also moved the '!' printing code under the stage_1 label instead of
    deleting it because it serves as a convenient way to check the computer
    actually gets to the stage 1 code, instead of branching to some
    unexpected place.
    
    The cld instruction was added because I'm not sure what the state of the
    direction flag is when the computer boots

--- stage_1.s
+++ stage_1.s
@@ -3,13 +3,27 @@
 .global stage_1_prelude
 
 stage_1_prelude:
-	jmp $0x0000, $(0x7c00 + mbr_boot)
+	jmp $0x0000, $(0x7c00 + relocate_stage_1)
 
-mbr_boot:
+relocate_stage_1:
+	cld
+	movw $0x0060, %ax
+	movw %ax, %es
+	xorw %di, %di
+
+	movw %di, %ds
+	movw $(0x7c00 + stage_1), %si
+
+	movw $(510 - (stage_1 - stage_1_prelude)), %cx
+	rep movs
+
+	jmp $0x0060, $0x0000
+
+stage_1:
 	movb $0x0e, %ah
 	movb $'!', %al
 	int $0x10
-	jmp mbr_boot
+	jmp stage_1
 
 .org 510
 .byte 0x55
