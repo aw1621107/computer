c30be2ad3db45baf3665692b051b3b50e4ae85e2
Alex Wang <aw1621107@gmail.com>
2017-10-26 21:15:41 -0400

    Implement basic string printing
    Simple loop to read a zero-terminated string and print it using the
    BIOS.
    
    Have to pay a code size penalty (for now) for hardcoding the printing of
    \r\n after a string is printed, but as long as the number of strings
    printed is greater than zero, the total savings will be positive.
    Chances are the savings won't be that great, though, since space in a
    bootloader is quite limited.
    
    Also created a "print" macro to help with readability and ease of use.
    It's nice not having to remember that the address of the string needs to
    be loaded into %ds:%si

--- stage_1.s
+++ stage_1.s
@@ -18,6 +18,33 @@ begin_stage_1_relocation:
 	jmp $0x0060, $(stage_1 - end_stage_1_relocation)
 end_stage_1_relocation:
 
+.macro print string_label
+	pushw %si
+	movw $(\string_label - (end_stage_1_relocation - begin_stage_1_relocation)), %si
+	call print_asciz_string
+	popw %si
+.endm
+
+print_asciz_string:
+	push %ax
+	movb $0x0e, %ah
+print_asciz_string_loop_begin:
+	lodsb
+	test %al, %al
+	jz print_asciz_string_done
+	int $0x10
+	jmp print_asciz_string_loop_begin
+print_asciz_string_done:
+	movb $0x0d, %al
+	int $0x10
+	movb $0x0a, %al
+	int $0x10
+	pop %ax
+	ret
+
+string_section_begin:
+string_section_end:
+
 stage_1:
 	movw %cs, %ax
 	movw %ax, %ds
